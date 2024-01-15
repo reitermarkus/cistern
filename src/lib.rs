9b43f44412c098719eaa8df442dfab899b8dd6b6
Markus Reiter <me@reitermark.us>
2024-01-15 04:02:44 +0100

    Update dependencies.

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,26 @@
-use ads1x1x::{channel, Ads1x1x, FullScaleRange::Within6_144V};
-use ads1x1x::interface::I2cInterface;
-use ads1x1x::ic::{Ads1115, Resolution16Bit};
+use ads1x1x::{FullScaleRange::Within6_144V, ChannelSelection};
+use ads1x1x::{Ads1115};
 use measurements::Length;
-use medianheap::MedianHeap;
+use medianheap::{Median, MedianHeap};
 use nb::block;
-use embedded_hal::adc::OneShot;
-use embedded_hal::blocking::i2c::WriteRead;
-use embedded_hal::blocking::i2c::Write;
+use embedded_hal::i2c::I2c;
 use webthing::{BaseThing, Thing, BaseProperty};
 use serde_json::json;
 
 pub struct Cistern<I> {
   heap: MedianHeap<i16>,
-  adc: Ads1x1x<I2cInterface<I>, Ads1115, Resolution16Bit, ads1x1x::mode::OneShot>,
+  adc: Ads1115<I, ads1x1x::mode::OneShot>,
 }
 
 impl<I, E> Cistern<I>
   where
-    I: Write<Error = E> + WriteRead<Error = E>,
+    I: I2c<Error = E>,
     E: std::fmt::Debug {
   const MAX_WATER_LEVEL: u16 = 156 - 21; // This is the height of the drain pipe.
   const MAX_VOLUME: u16 = 1600;
 
-  pub fn new(dev: I) -> Self {
-    let mut adc = Ads1x1x::new_ads1115(dev, Default::default());
+  pub fn new(i2c: I) -> Self {
+    let mut adc = Ads1115::new(i2c, Default::default());
     adc.set_full_scale_range(Within6_144V).expect("Failed to set voltage range");
 
     Self {
@@ -32,22 +29,23 @@ impl<I, E> Cistern<I>
     }
   }
 
-  pub fn measure(&mut self) -> Result<i16, ads1x1x::Error<E>> {
-    let value = block!(self.adc.read(&mut channel::SingleA0))?;
+  pub fn measure(&mut self) -> Result<Median<&i16>, E> {
+    let value = block!(self.adc.read(ChannelSelection::SingleA0))?;
     self.heap.push(value);
 
     Ok(self.heap.median().unwrap())
   }
 
   pub fn level(&self) -> Option<(f64, f64, f64)> {
-    self.heap.median().map(|value| {
+    self.heap.median_with(|v1, v2| (v1 + v2) / 2).map(|value| {
       let adjusted_value = if value < 0 {
-        0.0
+        0
       } else {
-        f64::from(value) * 6.144 / f64::from(2i16.pow(15) - 1)
+        i32::from(value) * 6_144 / (2i32.pow(15) - 1)
       };
 
-      let height = Length::from_millimeters((adjusted_value * 1000.0).round());
+      // Measurement range is 0-5 V for 5 m, so 1 mV is exactly 1 mm.
+      let height = Length::from_millimeters(adjusted_value as f64);
 
       let height = height.as_centimeters();
       let percent = if height > f64::from(Self::MAX_WATER_LEVEL) {
--- src/main.rs
+++ src/main.rs
@@ -1,93 +1,98 @@
 use std::env;
-use std::sync::{mpsc::channel, Arc, RwLock};
-use std::thread;
-use std::sync::Weak;
+use std::sync::{Arc, RwLock};
 
 use linux_embedded_hal::I2cdev;
-use simple_signal::{self, Signal};
 use serde_json::json;
-use webthing::{Thing, ThingsType, Action, server::ActionGenerator, WebThingServer};
+use webthing::{Thing, ThingsType, BaseActionGenerator, WebThingServer};
+use anyhow::Context;
+use tokio::{task, sync::oneshot};
 
 use cistern::Cistern;
 
-struct Generator;
-
-impl ActionGenerator for Generator {
-  fn generate(
-    &self,
-    _thing: Weak<RwLock<Box<dyn Thing>>>,
-    _name: String,
-    _input: Option<&serde_json::Value>,
-  ) -> Option<Box<dyn Action>> {
-    None
-  }
-}
-
 #[actix_rt::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> anyhow::Result<()> {
   env_logger::init();
 
-  let dev = I2cdev::new(env::var("I2C_DEVICE").expect("I2C_DEVICE is not set")).expect("Failed to open I2C device");
+  println!("YOLO");
 
-  let cistern = Cistern::new(dev);
+  println!("RUST_LOG={}", env::var("RUST_LOG").unwrap_or_default());
+
+  let i2c_device = env::var("I2C_DEVICE").context("I2C_DEVICE is not set")?;
+  let i2c = I2cdev::new(i2c_device).context("Failed to open I2C device")?;
+  let cistern = Cistern::new(i2c);
+
   let thing: Arc<RwLock<Box<dyn Thing + 'static>>> = Arc::new(RwLock::new(Box::new(cistern.to_thing())));
   let thing_clone = thing.clone();
 
-  let cistern = Arc::new(RwLock::new(cistern));
+  let cistern = Arc::new(tokio::sync::RwLock::new(cistern));
 
-  thread::spawn(move || {
-    let (sig_tx, sig_rx) = channel();
+  let (measurement_loop_stop_tx, mut measurement_loop_stop_rx) = oneshot::channel();
 
-    simple_signal::set_handler(&[Signal::Int], move |_| {
-      sig_tx.send(true).unwrap();
-    });
+  let measurement_loop = async {
+    task::spawn(async move {
+      log::info!("Starting measurement loop…");
 
-    let mut i = 0;
-    loop {
-      if sig_rx.try_recv().unwrap_or(false) {
-        break
+      let mut i = 0;
+      loop {
+        if measurement_loop_stop_rx.try_recv().unwrap_or(false) {
+          log::info!("Stopping measurement loop.");
+          break Ok(())
+        }
+
+        let mut cistern = cistern.write().await;
+        match cistern.measure() {
+          Ok(_) => {
+            i += 1;
+
+            // Don't update value after every measurement.
+            if i >= 1000 {
+              i = 0;
+
+              let (height, percentage, volume) = cistern.level().unwrap();
+
+              let mut thing = thing.write().unwrap();
+              let mut change_value = |property_name: &str, value| {
+                let property_name = property_name.to_owned();
+                let prop = thing.find_property(&property_name).unwrap();
+                let value = json!(value);
+                prop.set_cached_value(value.clone()).unwrap();
+                thing.property_notify(property_name, value);
+              };
+              change_value("height", height);
+              change_value("percentage", percentage * 100.0);
+              change_value("volume", volume);
+            }
+          },
+          Err(e) => break Err(e).context("Measurement failed"),
+        }
+
+        tokio::task::yield_now().await;
       }
+    }).await.context("Failed to start measurement loop")
+  };
 
-      let mut cistern = cistern.write().unwrap();
-      match cistern.measure() {
-        Ok(_) => {
-          i += 1;
+  let webthing_server = async {
+    let port = env::var("PORT").map(|p| p.parse::<u16>().expect("Failed to parse PORT")).unwrap_or(8888);
 
-          // Don't update value after every measurement.
-          if i >= 1000 {
-            i = 0;
-
-            let (height, percentage, volume) = cistern.level().unwrap();
-
-            let mut thing = thing.write().unwrap();
-            let mut change_value = |property_name: &str, value| {
-              let property_name = property_name.to_owned();
-              let prop = thing.find_property(&property_name).unwrap();
-              let value = json!(value);
-              prop.set_cached_value(value.clone()).unwrap();
-              thing.property_notify(property_name, value);
-            };
-            change_value("height", height);
-            change_value("percentage", percentage * 100.0);
-            change_value("volume", volume);
-          }
-        },
-        Err(e) => log::error!("Failed to measure: {:?}", e),
-      }
-    }
-
-    std::process::exit(1);
-  });
-
-  let mut server = WebThingServer::new(
+    let mut server = WebThingServer::new(
       ThingsType::Single(thing_clone),
-      Some(env::var("PORT").map(|p| p.parse::<u16>().expect("Failed to parse PORT")).unwrap_or(8888)),
+      Some(port),
       None,
       None,
-      Box::new(Generator),
+      Box::new(BaseActionGenerator),
       None,
       Some(true),
-  );
+    );
 
-  server.start(None).await
+    log::info!("Starting WebThing server on port {port}…");
+    let res = server.start(None).await;
+
+    measurement_loop_stop_tx.send(true).unwrap();
+
+    res.context("WebThing server failed")
+  };
+
+  tokio::try_join!(measurement_loop, webthing_server)?.0?;
+
+  Ok(())
 }
